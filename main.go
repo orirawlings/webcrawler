cfda8eda9a091e07bec800937ed2336143708fff
Ori Rawlings <orirawlings@gmail.com>
2016-10-26 16:27:21 -0500

    Add cpuprofiling capabilities and option

--- main.go
+++ main.go
@@ -19,32 +19,47 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
 )
 
-// Parse command line arguments. Returns the inital root URL and the
-// maximum depth of the crawl.
-func parseArgs() (startUrl string, depth int) {
-	// Override the default help usage message
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [-h] [options] URL\n", os.Args[0])
-		flag.PrintDefaults()
-	}
-	d := flag.Int("depth", 2, "The maximum depth of the web crawl")
-	flag.Parse()
+var (
+	depth   = flag.Int("depth", 2, "The maximum depth of the web crawl")
+	cpuprof = flag.String("cpuprofile", "", "Write cpu profile to `file`")
+)
+
+func startUrl() string {
 	if flag.NArg() < 1 {
 		fmt.Fprintln(os.Stderr, "Please provide an initial URL to start the crawl")
 		os.Exit(1)
 	}
-	return flag.Arg(0), *d
+	return flag.Arg(0)
 }
 
 func main() {
 	// Set logging time format
 	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
 
-	start, depth := parseArgs()
+	// Override the default help usage message
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [-h] [options] URL\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	// Enable cpuprofiling if requested
+	if *cpuprof != "" {
+		f, err := os.Create(*cpuprof)
+		if err != nil {
+			log.Fatal("Could not create CPU profile: ", err)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("Could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	done := make(chan struct{}) // Not used. This can be used cancel the crawl early.
-	statuses := Crawl(done, start, depth, NewFetch())
+	statuses := Crawl(done, startUrl(), *depth, NewFetch())
 	for status := range statuses {
 		if status.Err != nil {
 			log.Printf("%v\t%v\t%v\n", status.Status, status.Url, status.Err)
